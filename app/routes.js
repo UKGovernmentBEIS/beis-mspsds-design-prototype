7a6e8ae4fe92b64189ee2eac62944e2dfef08ebd
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-11-14 13:43:06 +0000

    Implement branching via router

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,25 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+// Process incoming report flow endpoints
+router.post('/flows/process-incoming/report-info-endpoint', function(req, res) {
+  let endpoint = req.session.data.new['issue-type']
+  switch (endpoint) {
+    case 'Enquiry':
+      res.redirect('/flows/process-incoming/question-info')
+      break;
+    case 'Product safety allegation':
+        res.redirect('/flows/process-incoming/allegation-info')
+        break;
+    case 'Product recall notification':
+        res.redirect('/flows/process-incoming/recall-info')
+        break;
+    case 'Notification from RAPEX':
+        res.redirect('/flows/process-incoming/rapex')
+        break;
+    default:
+      res.redirect(endpoint)
+  }
+})
