fdaf3c97200d3f1e0685bfe699577e822f88a3f0
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-11-19 14:59:05 +0000

    Back case-list with shared data, not hard-wired text

--- app/data/cases.js
+++ app/data/cases.js
@@ -1,6 +1,53 @@
+// Blueprint
+/*
+    {
+      type: 'Case',
+      status: 'Open',
+      id: '1811-xxxx',
+      title: 'Untitled case',
+      assignee: '',
+      dateCreated: '18/10/2018',
+      dateUpdated: '21/10/2018',
+      report: {
+        type: 'Allegation',
+        date: '18/10/2018',
+        summary: '',
+        otherDetails: '',
+        reporter: {
+          type: "Consumer",
+          name: "",
+          phoneNumber: "",
+          emailAddress: "",
+          otherDetails: ""
+        },
+        correspondence: {
+          type: 'Email',
+          subject: '',
+          body: '',
+          attachment: '',
+          attachmentDescription: ''
+        }
+      },
+      hazard: {
+        type: '',
+        details: '',
+        group: '',
+        riskLevel: '',
+        assessment: ''
+      },
+      incidents: [],
+      products: [],
+      businesses: [{id: 'b1', role: 'Manufacturer'}],
+      contacts: [{id: 'c3', role: 'Reporter'}],
+      attachments: [],
+      related: [],
+      activites: [],
+    }
+*/
 module.exports = {
   cases: [
     {
+      // TODO Remove this case once case__empty.js page is no longer needed
       type: 'Case',
       id: "1810-0001",
       title: "Untitled case",
@@ -16,9 +63,12 @@ module.exports = {
       priority: "Not set",
       numAttachments: 0,
       products: [],
-      activity: []
+      activity: [],
+      activites: [],
+      dateCreated: '18/10/2018',
     },
     {
+      // TODO Remove this case once case__empty.js page is no longer needed
       id: "1810-0002",
       type: "Question",
       title: "Charge Worx, Travel plug adaptor –Electric shock, fire hazard",
@@ -270,13 +320,6 @@ module.exports = {
       ]
 
     },
-
-
-
-
-
-
-
     {
       type: 'Case',
       status: 'Open',
@@ -606,6 +649,593 @@ module.exports = {
           action: ""
         }
       ]
-    }
+    },
+    {
+      type: 'Case',
+      status: 'Open',
+      id: '1811-0802',
+      title: 'Untitled case',
+      assignee: 'Ed Smith-Muller',
+      dateCreated: '18/10/2018',
+      dateUpdated: '21/10/2018',
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      products: ['p2'],
+    },
+    {
+      type: 'Question',
+      status: 'Open',
+      id: '1811-0801',
+      title: "Shouldn't this be categorised as a toy?",
+      assignee: 'Tim Harwood',
+      dateCreated: '18/10/2018',
+      dateUpdated: '21/10/2018',
+      products: ['p4'],
+    },
+    {
+      type: 'Case',
+      status: 'Open',
+      id: '1811-0800',
+      assignee: 'Tim Harwood',
+      dateCreated: '18/10/2018',
+      dateUpdated: '21/10/2018',
+      hazard: {
+        type: 'Entrapment, Injuries hazard',
+      },
+      products: ['p5'],
+    },
+    {
+      type: 'Case',
+      status: 'Open',
+      id: '1811-0799',
+      assignee: 'Nick Golding',
+      dateCreated: '18/10/2018',
+      dateUpdated: '20/10/2018',
+      hazard: {
+        type: 'Choking hazard',
+      },
+      products: ['p6'],
+    },
+    {
+      type: 'Case',
+      status: 'Open',
+      id: '1811-0798',
+      assignee: 'Christopher Hunter',
+      dateCreated: '18/10/2018',
+      dateUpdated: '21/10/2018',
+      hazard: {
+        type: 'Suffocation hazard',
+      },
+      products: ['p7'],
+    },
+    {
+      type: 'Question',
+      status: 'Open',
+      id: '1811-0797',
+      assignee: 'Amanda Farrell',
+      title: 'Has anyone successfully located the importer for these?',
+      dateCreated: '18/10/2018',
+      dateUpdated: '20/10/2018',
+      products: ['p8'],
+    },
+    {
+      type: 'Case',
+      status: 'Open',
+      id: '1811-0796',
+      assignee: 'Ed Smith-Muller',
+      title: 'Has anyone successfully located the importer for these?',
+      dateCreated: '18/10/2018',
+      dateUpdated: '20/10/2018',
+      hazard: {
+        type: 'Electric shock hazard',
+      },
+      products: ['p9'],
+    },
+    {
+      id: '1811-0795',
+      type: 'Case',
+      status: 'Open',
+      products: ['p10'],
+      hazard: {
+        type: 'Electric shock hazard',
+      },
+      assignee: 'Nick Golding',
+      team: 'Processing',
+      overdue: 'Overdue',
+      dateUpdated: '20/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0794',
+      type: 'Case',
+      status: 'Open',
+      products: ['p11'],
+      hazard: {
+        type: 'Chemical hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '20/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0793',
+      type: 'Question',
+      status: 'Open',
+      products: ['p12'],
+      title: 'What legislation applies to importing these?',
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0792',
+      type: 'Case',
+      status: 'Open',
+      products: ['p13'],
+      hazard: {
+        type: 'Damage to sight hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0791',
+      type: 'Case',
+      status: 'Open',
+      products: ['p14'],
+      hazard: {
+        type: 'Chemical, Choking hazard',
+      },
+      assignee: 'Christopher Hunter',
+      team: 'Processing',
+      overdue: 'Overdue',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0790',
+      type: 'Case',
+      status: 'Open',
+      products: ['p15'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Garry Oldboy',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0789',
+      type: 'Question',
+      status: 'Open',
+      products: ['p16'],
+      title: 'Haven\'t these been recalled already?',
+      assignee: 'Adebola Showemimo',
+      team: 'TSC',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0788',
+      type: 'Case',
+      status: 'Open',
+      products: ['p17'],
+      hazard: {
+        type: 'Poison hazard',
+      },
+      assignee: 'Tim Harwood',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0787',
+      type: 'Case',
+      status: 'Closed',
+      products: ['p18'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Nick Golding',
+      team: 'Processing',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0786',
+      type: 'Question',
+      status: 'Closed',
+      products: ['p19'],
+      title: 'Can anyone think of another title?',
+      assignee: 'Adebola Showemimo',
+      team: 'TSC',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0785',
+      type: 'Case',
+      status: 'Open',
+      products: ['p20'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Christopher Hunter',
+      team: 'Processing',
+      dateUpdated: '19/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0784',
+      type: 'Case',
+      status: 'Open',
+      products: ['p21'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0783',
+      type: 'Case',
+      status: 'Closed',
+      products: ['p22'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Christopher Hunter',
+      team: 'Processing',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0782',
+      type: 'Case',
+      status: 'Open',
+      products: ['p23'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Nick Golding',
+      team: 'Processing',
+      overdue: 'Overdue',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0781',
+      type: 'Case',
+      status: 'Closed',
+      products: ['p24'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0780',
+      type: 'Case',
+      status: 'Open',
+      products: ['p25'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0779',
+      type: 'Case',
+      status: 'Open',
+      products: ['p26'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      overdue: 'Overdue',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0778',
+      type: 'Question',
+      status: 'Open',
+      products: ['p27'],
+      title: 'What legislation should I be testing under?',
+      assignee: 'Adebola Showemimo',
+      team: 'TSC',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0777',
+      type: 'Case',
+      status: 'Open',
+      products: ['p28'],
+      hazard: {
+        type: 'Injuries hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0776',
+      type: 'Case',
+      status: 'Open',
+      products: ['p29'],
+      hazard: {
+        type: 'Entrapment, Injuries hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0775',
+      type: 'Case',
+      status: 'Open',
+      products: ['p30'],
+      hazard: {
+        type: 'Choking hazard',
+      },
+      assignee: 'Christopher Hunter',
+      team: 'Processing',
+      overdue: 'Overdue',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0774',
+      type: 'Case',
+      status: 'Open',
+      products: ['p31'],
+      hazard: {
+        type: 'Suffocation hazard',
+      },
+      assignee: 'Nick Golding',
+      team: 'Processing',
+      dateUpdated: '18/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0773',
+      type: 'Case',
+      status: 'Open',
+      products: ['p32'],
+      hazard: {
+        type: 'Strangulation hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0772',
+      type: 'Case',
+      status: 'Open',
+      products: ['p33'],
+      hazard: {
+        type: 'Electric shock hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0771',
+      type: 'Question',
+      status: 'Open',
+      products: ['p34'],
+      title: 'Toys (Safety) Regulations -can you plead guilty in absence?',
+      assignee: 'Nick Golding',
+      team: 'TSC',
+      overdue: 'Overdue',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0770',
+      type: 'Case',
+      status: 'Open',
+      products: ['p35'],
+      hazard: {
+        type: 'Chemical hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0769',
+      type: 'Case',
+      status: 'Open',
+      products: ['p36'],
+      hazard: {
+        type: 'Damage to sight hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0768',
+      type: 'Case',
+      status: 'Open',
+      products: ['p37'],
+      hazard: {
+        type: 'Damage to sight hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0767',
+      type: 'Case',
+      status: 'Open',
+      products: ['p38'],
+      hazard: {
+        type: 'Chemical, Choking hazard',
+      },
+      assignee: 'Christopher Hunter',
+      team: 'Processing',
+      overdue: 'Overdue',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0766',
+      type: 'Case',
+      status: 'Open',
+      products: ['p39'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Nick Golding',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0765',
+      type: 'Case',
+      status: 'Open',
+      products: ['p40'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0764',
+      type: 'Case',
+      status: 'Open',
+      products: ['p41'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0763',
+      type: 'Question',
+      status: 'Closed',
+      products: ['p42'],
+      hazard: {
+        type: 'Another title? Really?',
+      },
+      assignee: 'Amanda Farrell',
+      team: 'TSC',
+      dateUpdated: '17/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0762',
+      type: 'Case',
+      status: 'Closed',
+      products: ['p43'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '16/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0761',
+      type: 'Case',
+      status: 'Open',
+      products: ['p44'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Christopher Hunter',
+      team: 'Processing',
+      dateUpdated: '16/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0760',
+      type: 'Case',
+      status: 'Open',
+      products: ['p45'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '16/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0759',
+      type: 'Case',
+      status: 'Closed',
+      products: ['p46'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Christopher Hunter',
+      team: 'Processing',
+      dateUpdated: '16/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0758',
+      type: 'Question',
+      status: 'Open',
+      products: ['p47'],
+      title: 'Has anyone recently advised on snap bracelets / slap warps? ',
+      assignee: "Jan Mikolajczak",
+      team: 'TSC',
+      overdue: 'Overdue',
+      dateUpdated: '16/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0757',
+      type: 'Case',
+      status: 'Closed',
+      products: ['p48'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Ed Smith-Muller',
+      dateUpdated: '16/10/2018',
+      dateCreated: '18/10/2018'
+    },
+    {
+      id: '1811-0756',
+      type: 'Case',
+      status: 'Open',
+      products: ['p49'],
+      hazard: {
+        type: 'Electric shock, fire hazard',
+      },
+      assignee: 'Tim Harwood',
+      team: 'Processing',
+      dateUpdated: '16/10/2018',
+
+      dateCreated: '18/10/2018'
+    },
   ],
 }
--- app/data/products.js
+++ app/data/products.js
@@ -29,7 +29,6 @@ module.exports = {
             related: [],
             activity: []
         },
-
         {
             id: 'p2',
             name: 'Travel plug adaptor — Charge Worx 931L',
@@ -62,7 +61,190 @@ module.exports = {
         {
             id: 'p3',
             name: 'Another adaptor'
-        }
-
+        },
+        {
+            id: 'p4',
+            name: 'Spongebob Squarepants Toy scooter'
+        },
+        {
+            id: 'p5',
+            name: 'FLEXA, Children high chair 82-10020-1'
+        },
+        {
+            id: 'p6',
+            name: 'Bieco, Schnullerhalter, Soother holder'
+        },
+        {
+            id: 'p7',
+            name: 'BabyDan, Cuddle Nest Ergo'
+        },
+        {
+            id: 'p8',
+            name: 'Zhuzhu Rabbit, Childrens outfit'
+        },
+        {
+            id: 'p9',
+            name: 'BOLI Razor, Technology Quality Life, Electric razor'
+        },
+        {
+            id: 'p10',
+            name: 'NO ONE, Your Best Choice, Electric razor',
+        },
+        {
+            id: 'p11',
+            name: 'Aqua Sphere, Beachwalker XP, Water shoes',
+        },
+        {
+            id: 'p12',
+            name: 'CP, 3in 1 LASER & LED LIGHT, Laser pointer',
+        },
+        {
+            id: 'p13',
+            name: 'ZK, T2 Function Laser & LED Light with Carabiner Clip, Laser pointer',
+        },
+        {
+            id: 'p14',
+            name: 'Lovely Cartoon Unisex Quartz Sports Kids Wrist Watch, Children watch',
+        },
+        {
+            id: 'p15',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p16',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p17',
+            name: 'Multiple: Finger puppets',
+        },
+        {
+            id: 'p18',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p19',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p20',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p21',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p22',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p23',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p24',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p25',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p26',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p27',
+            name: 'Travel adaptors',
+        },
+        {
+            id: 'p28',
+            name: 'Spongebob Squarepants Toy scooter',
+        },
+        {
+            id: 'p29',
+            name: 'FLEXA, Children high chair 82-10020-1',
+        },
+        {
+            id: 'p30',
+            name: 'Bieco, Schnullerhalter, Soother holder',
+        },
+        {
+            id: 'p31',
+            name: 'BabyDan, Cuddle Nest Ergo',
+        },
+        {
+            id: 'p32',
+            name: 'Zhuzhu Rabbit, Childrens outfit',
+        },
+        {
+            id: 'p33',
+            name: 'BOLI Razor, Technology Quality Life, Electric razor',
+        },
+        {
+            id: 'p34',
+            name: 'Toys',
+        },
+        {
+            id: 'p35',
+            name: 'Aqua Sphere, Beachwalker XP, Water shoes',
+        },
+        {
+            id: 'p36',
+            name: 'CP, 3in 1 LASER & LED LIGHT, Laser pointer',
+        },
+        {
+            id: 'p37',
+            name: 'ZK, T2 Function Laser & LED Light with Carabiner Clip, Laser pointer',
+        },
+        {
+            id: 'p38',
+            name: 'Lovely Cartoon Unisex Quartz Sports Kids Wrist Watch, Children watch',
+        },
+        {
+            id: 'p39',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p40',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p41',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p42',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p43',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p44',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p45',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p46',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p47',
+            name: 'Red Zulu, Slap Wrap',
+        },
+        {
+            id: 'p48',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
+        {
+            id: 'p49',
+            name: 'Charge Worx, Travel plug adaptor',
+        },
     ]
 }
