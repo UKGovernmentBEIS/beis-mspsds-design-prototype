29752e3fff028b776e811c1038fe601da2b8f5dc
Tim Harwood <tim@timharwood.co.uk>
2018-11-20 17:26:55 +0000

    rationalising business panel; first fix related

--- app/data/businesses.js
+++ app/data/businesses.js
@@ -2,10 +2,25 @@ module.exports = {
     businesses: [
         {
             id: 'b1',
-            name: 'ChargeWorx',
-            products: ['p1', 'p2'],
-            cases: ['1811-0803'],
+            name:           'Charge Worx UK Ltd',
+            tradingName:    'Charge Worx',
+            
+            companyNumber:  '36534564658',
+            vatNumber:      'GB220430231',
+            companyType:    'Public limited company',
 
+            sicCode:            '47110 - Retail sale in non-specialised stores with food, beverages or tobacco predominating',
+            primaryAuthority:   'Shropshire County Council',
+            additionalInfo:     'Charge Wrox UK, is a British subsiduary of and American company with headquarters in Welwyn Garden City, Hertfordshire, England, United Kingdom.',
+
+            address:            '48 Bush Road,  Welwyn Garden City, Hertfordshire, England, United Kingdom  WG1 1XQ',
+
+            posterImage: '/public/images/ChargeWox_Logo.png',
+
+            contact:        'Paul Jones, 0207 1234 5678, paul.jones@abc.np',
+
+            products:   ['p1', 'p2'],
+            cases:      ['1811-0803'],
             attachments: ['a1'],
 
             contacts: [
@@ -23,6 +38,10 @@ module.exports = {
             ]
 
         },
+
+
+
+        
         {
             id: 'b2',
             name:           'Wonderlite Ltd.',
@@ -31,6 +50,17 @@ module.exports = {
             address:        '48 Bush Road, London N1 1XQ',
             contact:        'Paul Jones, 0207 1234 5678, paul.jones@abc.np',
             primaryAuthority: 'Suffolk Trading Standards'
+        },
+        {
+            id: 'b3',
+            name:           'Red Zulu Ltd',
+            tradingName:    'Red Zulu',
+            companyNumber:  '00445790',
+            address:        '48 Bush Road, London N1 1XQ',
+            posterImage:    '/public/images/redzulu.gif',
+
+            contact:        'Paul Jones, 0207 1234 5678, paul.jones@abc.np',
+            primaryAuthority: 'Shropshire County Council'
         }
     ]
 };
