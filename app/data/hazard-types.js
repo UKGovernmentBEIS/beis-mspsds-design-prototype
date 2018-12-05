ff928415c538704d14e22a23cc3c630facce0f0d
Tim Harwood <tim@timharwood.co.uk>
2018-12-05 11:15:00 +0000

    added hazard type list; updated file inputs to make saving simpler

--- app/data/hazard-types.js
+++ app/data/hazard-types.js
@@ -1,6 +1,26 @@
 module.exports = {
     hazardTypes: [
-      'Electric shock',
-      'Fire hazard'
+      	'Asphyxiation',
+		'Burns',
+		'Chemical',
+		'Choking',
+		'Cuts',
+		'Damage to hearing',
+		'Damage to sight',
+		'Drowning',
+		'Electric shock',
+		'Electromagnetic disturbance',
+		'Energy consumption',
+		'Entrapment',
+		'Environment',
+		'Fire',
+		'Health risk/other',
+		'Injuries',
+		'Incorrect measurement',
+		'Microbiological',
+		'Security',
+		'Strangulation',
+		'Suffocation',
+		'Other'
     ]
 };
