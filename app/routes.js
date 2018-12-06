3444720f26e4b52ec130f640987ac2ea5197e4e3
MatthewBurstein <matthew.burstein@softwire.com>
2018-12-06 12:20:08 +0000

    Capitalise attachment titles in create case flow

--- app/routes.js
+++ app/routes.js
@@ -66,9 +66,9 @@ router.post('/:mode/flows/ts-create/save', function (req, res) {
     newCase.title = newCase.report.productType + " - " + newCase.report.hazardType;
   }
 
-  const testFile = attachment.build({ title: "Test results", filename: data.new.files.testing.upload })
-  const riskFile = attachment.build({ title: "Risk assessment", filename: data.new.files.risk.upload })
-  const relatedFile = attachment.build({ title: "Related file", filename: data.new.files.related.upload })
+  const testFile = attachment.build({ title: "Test Results", filename: data.new.files.testing.upload })
+  const riskFile = attachment.build({ title: "Risk Assessment", filename: data.new.files.risk.upload })
+  const relatedFile = attachment.build({ title: "Related attachment", filename: data.new.files.related.upload })
   newCase.attachments.unshift(testFile.id, riskFile.id, relatedFile.id)
   data.attachments.push(testFile, riskFile, relatedFile)
 
