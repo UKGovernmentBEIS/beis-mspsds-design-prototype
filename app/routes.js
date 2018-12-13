68a16b6e775619425dcd4bd4e97a8c037b566b36
kicferk1 <michal.kreft@softwire.com>
2018-12-13 10:34:29 +0000

    Move correspondence to pages

--- app/routes.js
+++ app/routes.js
@@ -395,17 +395,14 @@ router.post('/:mode/flows/add-activity/choose', function (req, res) {
     case 'comment':
       res.redirect("/root/flows/add-comment/01-add-comment");
       break;
-    case 'correspondence':
-      res.redirect("/flows/add-correspondence/01-add-correspondence-context.html");
-      break;
     case 'email':
-      res.redirect("/flows/add-correspondence/01e-add-email-context.html");
+      res.redirect("/root/flows/add-correspondence/01e-add-email-context.html");
       break;
     case 'phoneCall':
-      res.redirect("/flows/add-correspondence/01p-add-phonecall-context.html");
+      res.redirect("/root/flows/add-correspondence/01p-add-phonecall-context.html");
       break;
     case 'meeting':
-      res.redirect("/flows/add-correspondence/01m-add-meeting-context.html");
+      res.redirect("/root/flows/add-correspondence/01m-add-meeting-context.html");
       break;
     case 'contact':
       res.redirect("/root/flows/contact/add/01.html");
