78b1952f0b58424f8b084ff6aa761f5ce015a265
Tim Harwood <tim@timharwood.co.uk>
2019-01-16 14:03:50 +0000

    ts ask a question

--- app/utils/activity.js
+++ app/utils/activity.js
@@ -3,11 +3,11 @@ const activityTemplates = require("../data/activities/templates")
 
 buildAddAttachment = function (attachment, user) {
     return activityTemplates.addAttachment({
-      author: user,
-      title: attachment.title,
-      description: attachment.description,
-      isImage: attachment.isImage,
-      fileExtension: attachment.fileExtension
+      author:           user,
+      title:            attachment.title,
+      description:      attachment.description,
+      isImage:          attachment.isImage,
+      fileExtension:    attachment.fileExtension
     });
 }
 
@@ -15,38 +15,38 @@ module.exports = {
     buildCreateCase: function (kase) {
         const caseCreatedActivity = activityTemplates.caseCreated;
         return caseCreatedActivity({
-            caseType: kase.report.type,
-            caseTitle: kase.title,
-            author: kase.assignee,
-            dateCreated: kase.dateCreated,
-            reporterName: (kase.report.reporter) ? kase.report.reporter.name : "",
-            reporterType: (kase.report.reporter) ? kase.report.reporter.type : "",
-            reporterPhoneNumber: (kase.report.reporter) ? kase.report.reporter.phoneNumber : "",
-            reporterEmailAddress: (kase.report.reporter) ? kase.report.reporter.emailAddress : "",
-            reporterOtherDetails: (kase.report.reporter) ? kase.report.reporter.otherDetails : "",
-            productType: kase.report.productType,
-            hazardType: kase.report.hazardType,
-            caseSummary: kase.report.summary
+            caseType:               kase.report.type,
+            caseTitle:              kase.title,
+            author:                 kase.assignee,
+            dateCreated:            kase.dateCreated,
+            reporterName:           (kase.report.reporter) ? kase.report.reporter.name : "",
+            reporterType:           (kase.report.reporter) ? kase.report.reporter.type : "",
+            reporterPhoneNumber:    (kase.report.reporter) ? kase.report.reporter.phoneNumber : "",
+            reporterEmailAddress:   (kase.report.reporter) ? kase.report.reporter.emailAddress : "",
+            reporterOtherDetails:   (kase.report.reporter) ? kase.report.reporter.otherDetails : "",
+            productType:            kase.report.productType,
+            hazardType:             kase.report.hazardType,
+            caseSummary:            kase.report.summary
         });
     },
     buildAddProduct: function (product, user) {
         const activityTemplate = activityTemplates.addProduct;
         return activityTemplate({
-            author: user,
-            date: today.long(),
-            productName: product.name,
-            productType: product.type,
-            productCategory: product.category,
-            productCode: product.code,
+            author:             user,
+            date:               today.long(),
+            productName:        product.name,
+            productType:        product.type,
+            productCategory:    product.category,
+            productCode:        product.code,
             productDescription: product.description,
-            productImage: product.posterImage
+            productImage:       product.posterImage
         });
     },
     buildAddBusiness: function (business, user) {
         const activityTemplate = activityTemplates.addBusiness;
         return activityTemplate({
-            author: user,
-            date: today.long(),
+            author:         user,
+            date:           today.long(),
             businessName:           business.name,
             businessType:           business.type,
             businessAddress:        business.address,
@@ -57,10 +57,10 @@ module.exports = {
     buildChangeStatus : function(reqBody, user) {
         const changeStatusActivityTemplate = activityTemplates.changedStatus;
         return changeStatusActivityTemplate({
-          status: reqBody.status,
-          description: reqBody['status-description'],
-          author: user,
-          date: today.long()
+          status:       reqBody.status,
+          description:  reqBody['status-description'],
+          author:       user,
+          date:         today.long()
         });
     }
 }
