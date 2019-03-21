116a6fbdfb1a07042a10e6769cd88a5ba66efded
Edward Horsford <mail@edwardhorsford.com>
2019-03-21 16:03:32 +0000

    Fix menu js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,21 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
+
+new HMCTSFrontend.Menu({
+    container: $('.hmcts-menu--add-new'),
+    mq: '(min-width: 200em)',
+    buttonText: 'Add new',
+    buttonClasses: 'hmcts-button--secondary hmcts-menu__toggle-button--secondary',
+    menuClasses: 'hmcts-menu__wrapper--right'
+  });
+
+
+
+  new HMCTSFrontend.Menu({
+    container: $('.hmcts-menu--actions'),
+    mq: '(min-width: 200em)',
+    buttonText: 'Actions',
+    buttonClasses: 'hmcts-button--secondary hmcts-menu__toggle-button--secondary',
+    menuClasses: 'hmcts-menu__wrapper--right'
+  });
