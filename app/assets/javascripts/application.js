85990973e7886d26a9b353dbf9afb8297a734494
Edward Horsford <mail@edwardhorsford.com>
2019-04-05 16:52:11 +0100

    Draft designs for new case pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 /* global $ */
 
 // Warn about using the kit in production
@@ -10,20 +11,44 @@ $(document).ready(function () {
 })
 
 
-new HMCTSFrontend.Menu({
+if ($('.hmcts-menu--add-new').length){
+  new HMCTSFrontend.Menu({
     container: $('.hmcts-menu--add-new'),
     mq: '(min-width: 200em)',
     buttonText: 'Add new',
     buttonClasses: 'hmcts-button--secondary hmcts-menu__toggle-button--secondary',
     menuClasses: 'hmcts-menu__wrapper--right'
   });
+}
 
 
+if ($('.hmcts-menu--actions').length){
+  new HMCTSFrontend.Menu({
+    container: $('.hmcts-menu--actions'),
+    mq: '(min-width: 200em)',
+    buttonText: 'Actions',
+    buttonClasses: 'hmcts-button--secondary hmcts-menu__toggle-button--secondary',
+    menuClasses: 'hmcts-menu__wrapper--right'
+  });
+}
 
-new HMCTSFrontend.Menu({
-  container: $('.hmcts-menu--actions'),
-  mq: '(min-width: 200em)',
-  buttonText: 'Actions',
-  buttonClasses: 'hmcts-button--secondary hmcts-menu__toggle-button--secondary',
-  menuClasses: 'hmcts-menu__wrapper--right'
-});
+$('.opss-filter--hide').click(function(e){
+  e.preventDefault()
+
+  button = $(this)
+
+  if (button.text()== '+ Show case filters'){
+    $('.mspsds-case-filter-controls').show()
+    button.text("- Hide case filters")
+    $('.mspsds-case-list-tab').removeClass('govuk-grid-column-full')
+    $('.mspsds-case-list-tab').addClass('govuk-grid-column-three-quarters')
+
+  }
+  else {
+    $('.mspsds-case-filter-controls').hide()
+    button.text("+ Show case filters")
+    $('.mspsds-case-list-tab').removeClass('govuk-grid-column-three-quarters')
+    $('.mspsds-case-list-tab').addClass('govuk-grid-column-full')
+  }
+
+})
