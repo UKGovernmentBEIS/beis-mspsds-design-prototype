d906639e4605fd71c934e425c25af91ea6422682
Edward Horsford <mail@edwardhorsford.com>
2019-08-12 16:41:16 +0100

    Misc updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,6 +32,16 @@ if ($('.hmcts-menu--actions').length){
   });
 }
 
+if ($('.hmcts-menu--add-case-history').length){
+  new HMCTSFrontend.Menu({
+    container: $('.hmcts-menu--add-case-history'),
+    mq: '(min-width: 300em)',
+    buttonText: 'Add new',
+    buttonClasses: 'hmcts-button--secondary hmcts-menu__toggle-button--secondary'
+  });
+}
+
+
 $('.opss-filter--hide').click(function(e){
   e.preventDefault()
 
@@ -73,3 +83,88 @@ $('.app-toggle-link').click(function(e){
   targetItems.toggleClass('hidden', 0)
 
 })
+
+  // Deselect main checkboxes when none ticked
+  // Deselect none when main checkboxes ticked
+  var $selectButtons = $('*[data-js-select="select"]')
+  var $deSelectButtons = $('*[data-js-select="deselect"]')
+
+  $selectButtons.on('click', function () {
+    $deSelectButtons.each((index, item) => {
+      if($(item).is(":checked")){
+        // item.click()
+        $(item).next().click()
+      }
+    })
+    // $(this).click()
+  })
+  $deSelectButtons.on('click', function () {
+
+      $selectButtons.each((index, item) => {
+        if($(item).is(":checked")){
+          // item.click()
+          $(item).next().click()
+        }
+      })
+
+  })
+
+// Ignore clicks on # anchors
+ $('a[href=#]').on('click', function(e){
+  e.preventDefault()
+ })
+
+
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.className == "moj-multi-file-upload__dropzone") {
+    $(event.target).addClass("moj-multi-file-upload--dragover")
+    // event.target.style.background = "grey";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.className == "moj-multi-file-upload__dropzone") {
+    event.target.style.background = "";
+    $(event.target).removeClass("moj-multi-file-upload--dragover")
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.className == "moj-multi-file-upload__dropzone") {
+    // event.target.style.background = "";
+    $(event.target).removeClass("moj-multi-file-upload--dragover")
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
